35e7f0654116d9901a025f0171fce9c96970ad5a
James A. Brannan <jamesabrannan@hushmail.com>
2025-07-30 16:18:26 -0400

    Create MultiplePages.vb

---
+++ print-manager-dotnet-core-vb/MultiplePages.vb
@@ -0,0 +1,17 @@
+﻿Imports ceTe.DynamicPDF.Printing
+
+Namespace print_manager_dotnet_core_vb
+    Class MultiplePages
+
+        Public Shared Sub Run()
+            Dim printJob As New PrintJob(Printer.Default)
+            printJob.Pages.Add(Util.GetPath("Resources/DocumentA1.pdf"))
+            printJob.Pages.Add(Util.GetPath("Resources/DocumentB1.pdf"))
+            printJob.Pages.Add(Util.GetPath("Resources/DocumentC1.pdf"))
+            printJob.Pages.Add(Util.GetPath("Resources/DocumentD1.pdf"))
+
+            printJob.Print()
+        End Sub
+
+    End Class
+End Namespace
