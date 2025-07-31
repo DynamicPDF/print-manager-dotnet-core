54ccac319d861ba29e04313b1090207d96982698
James A. Brannan <jamesabrannan@hushmail.com>
2025-07-31 11:29:02 -0400

    Create ByteArrayExample.vb

---
+++ print-manager-dotnet-core-vb/ByteArrayExample.vb
@@ -0,0 +1,14 @@
+﻿Imports ceTe.DynamicPDF.Printing
+
+Namespace print_manager_dotnet_core_vb
+    Class ByteArrayExample
+        Public Shared Sub Run()
+            Dim pdfBytes As Byte() = System.IO.File.ReadAllBytes(Util.GetPath("Resources/DocumentA.pdf"))
+
+            Dim pdf As New InputPdf(pdfBytes)
+            Dim printJob As New PrintJob(Printer.Default, pdf)
+
+            printJob.Print()
+        End Sub
+    End Class
+End Namespace
