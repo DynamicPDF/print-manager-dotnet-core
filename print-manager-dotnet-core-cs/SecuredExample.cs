38a60460584f8270cc63683d25547f355ef70661
jamesabrannan@hushmail.com <jamesabrannan@hushmail.com>
2026-05-20 13:35:06 -0400

    added security example

---
+++ print-manager-dotnet-core-cs/SecuredExample.cs
@@ -0,0 +1,15 @@
+﻿
+using ceTe.DynamicPDF.Printing;
+
+namespace print_manager_dotnet_core_cs
+{
+    internal class SecuredExample
+    {
+        public static void Run()
+        {
+            InputPdf inputPdf = new InputPdf(Util.GetPath("Resources/secured.pdf"), "password");
+            PrintJob printJob = new PrintJob(Printer.Default, inputPdf);
+            printJob.Print();
+        }       
+    }
+}
---
+++ print-manager-dotnet-core-vb/SecuredExample.vb
@@ -0,0 +1,10 @@
+﻿Imports ceTe.DynamicPDF.Printing
+
+Friend Class SecuredExample
+        Public Shared Sub Run()
+            Dim inputPdf As New InputPdf(Util.GetPath("Resources/secured.pdf"), "password")
+            Dim printJob As New PrintJob(Printer.Default, inputPdf)
+
+            printJob.Print()
+        End Sub
+End Class
