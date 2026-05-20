3c5f6ae787349f17d2fcadf421c3839c2fd1a2b6
jamesabrannan@hushmail.com <jamesabrannan@hushmail.com>
2026-05-20 16:47:15 -0400

    input source examples

---
+++ print-manager-dotnet-core-cs/InputSourceExample.cs
@@ -0,0 +1,20 @@
+﻿
+using ceTe.DynamicPDF.Printing;
+using System;
+
+namespace print_manager_dotnet_core_cs
+{
+    internal class InputSourceExample
+    {
+        public static void Run()
+        {
+            InputPdf inputPdf = new InputPdf(Util.GetPath("Resources/DocumentA.pdf"), "password");
+            Console.WriteLine("==== properties ====");
+            Console.WriteLine("Author: " + inputPdf.Author);
+            Console.WriteLine("Keywords: " + inputPdf.Keywords);
+            Console.WriteLine("Title: " + inputPdf.Title);
+            PrintJob printJob = new PrintJob(Printer.Default, inputPdf);
+            printJob.Print();
+        }
+    }
+}
---
+++ print-manager-dotnet-core-vb/InputSourceExample.vb
@@ -0,0 +1,15 @@
+﻿Imports ceTe.DynamicPDF.Printing
+Imports System
+Friend Class InputSourceExample
+    Public Shared Sub Run()
+        Dim inputPdf As New InputPdf(Util.GetPath("Resources/DocumentA.pdf"), "password")
+
+        Console.WriteLine("==== properties ====")
+        Console.WriteLine("Author: " & inputPdf.Author)
+        Console.WriteLine("Keywords: " & inputPdf.Keywords)
+        Console.WriteLine("Title: " & inputPdf.Title)
+
+        Dim printJob As New PrintJob(Printer.Default, inputPdf)
+        printJob.Print()
+    End Sub
+End Class
