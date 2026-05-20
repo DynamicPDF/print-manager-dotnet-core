3561b96f50ab1a8e0ded0303c488dd2c2d4e853a
jamesabrannan@hushmail.com <jamesabrannan@hushmail.com>
2026-05-20 17:11:16 -0400

    input pages example

---
+++ print-manager-dotnet-core-cs/InputPagesExample.cs
@@ -0,0 +1,21 @@
+﻿
+
+using ceTe.DynamicPDF.Printing;
+
+namespace print_manager_dotnet_core_cs
+{
+    internal class InputPagesExample
+    {
+        public static void Run()
+        {
+
+            PrintJob printJob = new PrintJob(Printer.Default);
+            InputPdf inputPdf = new InputPdf(Util.GetPath("Resources/DocumentA.pdf"));
+            InputPdfPage firstPage = inputPdf.Pages[0];
+            InputPdfPage fourthPage = inputPdf.Pages[3];
+            printJob.Pages.Add(firstPage);
+            printJob.Pages.Add(fourthPage);
+            printJob.Print();
+        }
+    }
+}
---
+++ print-manager-dotnet-core-vb/InputPagesExample.vb
@@ -0,0 +1,3 @@
+﻿Public Class InputPagesExample
+
+End Class
